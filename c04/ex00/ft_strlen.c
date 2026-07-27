5a99848fb3e916074c010b5fb9e042eabb0eb2dd
AymaneImr <elamraniayman389@gmail.com>
2026-07-27 17:28:52 +0100

    added c04: ex00 ex01 ex02 ex03

---
+++ c04/ex00/ft_strlen.c
@@ -0,0 +1,7 @@
+int ft_strlen(char *str) {
+  int len;
+
+  while (*(str + len) != '\0')
+    len++;
+  return (len);
+}
---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,20 @@
+int ft_atoi(char *str) {
+  int sign = 1;
+  int result = 0;
+
+  while (*str == ' ' || *str >= 9 && *str <= 13) {
+    str++;
+  }
+  while (*str == '-' || *str == '+') {
+    if (*str == '-') {
+      sign = -sign;
+    }
+    str++;
+  }
+
+  while (*str >= '0' && *str <= '9') {
+    result = result * 10 + (*str - '0');
+    str++;
+  }
+  return result * sign;
+}
